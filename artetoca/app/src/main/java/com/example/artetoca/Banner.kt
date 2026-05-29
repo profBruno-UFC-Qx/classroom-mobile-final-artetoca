3253dc73aeb7e2fc847172410ea62b38f7c3f0a2
Talles andré <tallesandre15@gmail.com>
2026-05-29 11:01:03 -0300

    Novo Componente Banner

---
+++ artetoca/app/src/main/java/com/example/artetoca/Banner.kt
@@ -0,0 +1,62 @@
+package com.example.artetoca
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Banner(modifier: Modifier) {
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(300.dp)
+            .background(Brush.verticalGradient(listOf(Color(0xffe98ba0), Color(0xfffdd5de))))
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.artetoca),
+                contentDescription = null,
+                modifier = Modifier.size(80.dp),
+            )
+            Text(
+                text = "Nossos Artesãos",
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.White,
+                modifier = Modifier.padding(top = 16.dp)
+            )
+            Text(
+                text = "Conheçam os talentos que produzem as obras",
+                style = MaterialTheme.typography.bodyLarge,
+                color = Color.White.copy(alpha = 0.8f),
+                modifier = Modifier.padding(top = 8.dp)
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun BannerPreview() {
+    Banner(Modifier)
+}
