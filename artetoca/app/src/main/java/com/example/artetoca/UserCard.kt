9183afc4f4053671a5250ed028168a340ebc977f
Talles andré <tallesandre15@gmail.com>
2026-05-29 11:02:08 -0300

    refatoração de componente

--- artetoca/app/src/main/java/com/example/artetoca/UserCard.kt
+++ artetoca/app/src/main/java/com/example/artetoca/UserCard.kt
@@ -2,21 +2,21 @@ package com.example.artetoca
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
-
 
 val cards = listOf(
     Triple("titulo", "categoria", "descriçao"),
@@ -28,27 +28,43 @@ val cards = listOf(
 )
 
 @Composable
-fun Card(titulo: String, especialidade: String, descrição: String, modifier: Modifier = Modifier) {
-    Box(
-        modifier
+fun UserCard(
+    titulo: String,
+    especialidade: String,
+    descrição: String,
+    modifier: Modifier = Modifier
+) {
+    Card (
+        modifier = modifier
             .fillMaxWidth()
             .padding(16.dp)
-            .background(color = Color(0xffffffff))
+//            .clip(RoundedCornerShape(16.dp))
+            .background(Color.White)
     ) {
         Row(
             horizontalArrangement = Arrangement.spacedBy(12.dp),
-            modifier = modifier.padding(10.dp)
+            modifier = Modifier.padding(10.dp)
         ) {
             AsyncImage(
                 model = "https://img.icons8.com/3d-fluent/100/user-2.png",
-                contentDescription = null, modifier = Modifier.height(100.dp)
+                contentDescription = null,
+                modifier = Modifier.size(100.dp)
             )
 
             Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
-                Text(text = titulo, fontSize = 16.sp)
+                Text(text = titulo, fontSize = 16.sp, fontWeight = FontWeight.Bold)
                 Text(text = especialidade, color = Color(0xffe88ba0))
-                Text(text = descrição, color = Color(0xff6d6d6d))
+                Text(text = descrição, color = Color(0xff6d6d6d), fontSize = 13.sp)
             }
+        }
+    }
+}
+
+@Composable
+fun CardMock() {
+    Column(Modifier.background(color = Color(0xfffef5f5))) {
+        cards.forEach {
+            UserCard(it.first, it.second, it.third)
         }
     }
 }
@@ -56,10 +72,5 @@ fun Card(titulo: String, especialidade: String, descrição: String, modifier: M
 @Preview
 @Composable
 fun CardPreview() {
-    Column(Modifier.background(color = Color(0xfffef5f5))) {
-        cards.forEach {
-            Card(it.first, it.second, it.third)
-        }
-    }
-
+    CardMock()
 }
