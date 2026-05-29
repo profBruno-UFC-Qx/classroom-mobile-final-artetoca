9b23ec98738c752d29d24ba3aa2adc226465470c
Talles andré <tallesandre15@gmail.com>
2026-05-29 09:43:02 -0300

    add scaffold

--- artetoca/app/src/main/java/com/example/artetoca/MainActivity.kt
+++ artetoca/app/src/main/java/com/example/artetoca/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
 import com.example.artetoca.ui.theme.ArtetocaTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,8 +14,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ArtetocaTheme {
-                PaginaInicial()
+            Scaffold {
+                ArtetocaTheme {
+                    PaginaInicial(Modifier.padding(it))
+                }
             }
         }
     }
