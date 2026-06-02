d91b160ab09c6131a4c789179295435a4e00cdd5
Talles andré <tallesandre15@gmail.com>
2026-06-02 11:42:12 -0300

    melhoramento no layout da tela do carrinho

--- artetoca/app/src/main/java/com/example/artetoca/carrinho.kt
+++ artetoca/app/src/main/java/com/example/artetoca/carrinho.kt
@@ -1,16 +1,22 @@
 package com.example.artetoca
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.IconButtonColors
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedIconButton
 import androidx.compose.material3.Text
@@ -28,13 +34,15 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun Car(modifier: Modifier = Modifier) {
-    var valor by remember { mutableIntStateOf(1) }
+fun ItemCard(preco:Float,qtd:Int,modifier: Modifier = Modifier) {
+    var valor by remember { mutableIntStateOf(qtd) }
     Row(
         modifier = modifier
             .fillMaxWidth()
-            .background(Color.White)
-            .padding(16.dp)
+            .height(IntrinsicSize.Max)
+            .background(Color.White, shape = MaterialTheme.shapes.medium)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Image(
             painter = painterResource(id = R.drawable.artetoca),
@@ -43,41 +51,57 @@ fun Car(modifier: Modifier = Modifier) {
                 .size(80.dp)
                 .clip(MaterialTheme.shapes.extraLarge)
         )
-        Column {
+        Column(
+            verticalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+        ) {
             Text("Item 1")
             Row(
                 verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceBetween
             ) {
                 Row(
-                    verticalAlignment = Alignment.CenterVertically
+                    verticalAlignment = Alignment.CenterVertically,
                 ) {
                     OutlinedIconButton(
                         onClick = { valor -= 1 },
-                        enabled = true
+                        enabled = true,
+                        border = BorderStroke(1.dp, Color.Transparent),
+                        colors = IconButtonColors(
+                            Color(0xfffce4e9),
+                            Color(0xff000000),
+                            Color(0xfffce4e9),
+                            Color(0xfffce4e9)
+                        )
                     ) { Text("-") }
 
                     Text(
-                        text = "${valor}",
+                        text = "$valor",
                         modifier = Modifier.padding(horizontal = 8.dp),
                         style = MaterialTheme.typography.bodyLarge
                     )
 
                     OutlinedIconButton(
-                        onClick = { valor += 1 }
+                        onClick = { valor += 1 },
+                        border = BorderStroke(1.dp, Color.Transparent),
+                        colors = IconButtonColors(
+                            Color(0xfffce4e9),
+                            Color(0xff000000),
+                            Color(0xfffce4e9),
+                            Color(0xfffce4e9)
+                        )
                     ) { Text("+") }
                 }
-                Text("R$ ${200}")
+                Text("R$ ${preco}")
                 IconButton(
                     onClick = {},
-                    modifier = Modifier
-                        .border(2.dp, Color.Red, MaterialTheme.shapes.medium)
                 ) {
                     Icon(
-                        painter = painterResource(id = R.drawable.red_trash),
+                        painter = painterResource(id = R.drawable.trash),
                         contentDescription = "Remover item",
                         modifier = Modifier.size(24.dp),
-//                        tint = Color.RED
+                        tint = Color(0xffd4183d)
                     )
                 }
             }
@@ -85,12 +109,24 @@ fun Car(modifier: Modifier = Modifier) {
     }
 }
 
+
+@Composable
+fun CardListMock() {
+    val scrollState = rememberScrollState()
+    Column(
+        modifier = Modifier
+            .background(Color(0xfffef5f5))
+            .padding(16.dp)
+            .verticalScroll(scrollState), verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        repeat(10) {
+            ItemCard(300f,1)
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun CarPreview() {
-    Column(modifier = Modifier.padding(16.dp)) {
-        repeat(10) {
-            Car()
-        }
-    }
+    CardListMock()
 }
