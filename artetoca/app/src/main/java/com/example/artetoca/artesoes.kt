e9fbcf3a1097d48f23a0033470c4bed158b1a23c
Talles andré <tallesandre15@gmail.com>
2026-06-02 11:43:15 -0300

    criação do componente finalcard na tela de artesoes

--- artetoca/app/src/main/java/com/example/artetoca/artesoes.kt
+++ artetoca/app/src/main/java/com/example/artetoca/artesoes.kt
@@ -14,37 +14,49 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun FinalCard(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxSize()
+            .height(150.dp)
+            .background(Color(0xE1FDD7E0), shape = MaterialTheme.shapes.medium),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Faça parte da nossa família",
+            style = MaterialTheme.typography.headlineSmall,
+            color = Color(0xffec9daf),
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = "Quer se tornar um artesão da Artetoca? Entre em contato conosco!",
+            style = MaterialTheme.typography.bodyLarge,
+            color = Color(0xff6c6b6b).copy(0.8f),
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(top = 8.dp, start = 24.dp, end = 24.dp)
+        )
+    }
+}
 
 @Composable
 fun Artesoes(modifier: Modifier = Modifier) {
     val scrollState = rememberScrollState()
-    Column(modifier = modifier.verticalScroll(scrollState).background(color = Color(0xfffef5f5))) {
-        Banner(modifier)
-        CardMock(modifier)
-        Column(
-            modifier = modifier
-                .padding(16.dp)
-                .fillMaxSize()
-                .height(150.dp)
-                .background(Color(0xfffdd7e0), shape = MaterialTheme.shapes.medium),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            Text(
-                text = "Faça parte da nossa família",
-                style = MaterialTheme.typography.headlineMedium,
-                color = Color(0xffec9daf),
-                modifier = Modifier.padding(top = 8.dp)
-            )
-            Text(
-                text = "Quer se tornar um artesão da Artetoca? Entre em contato conosco!",
-                style = MaterialTheme.typography.bodyLarge,
-                color = Color(0xff6c6b6b).copy(0.8f),
-                modifier = Modifier.padding(top = 8.dp)
-            )
-        }
+    Column(
+        modifier = modifier
+            .verticalScroll(scrollState)
+            .background(color = Color(0xfffef5f5))
+    ) {
+        Banner()
+        CardMock()
+        FinalCard()
     }
 }
 
