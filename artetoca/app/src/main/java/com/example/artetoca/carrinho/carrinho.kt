72bd5c90e2969f043b6dc86d0f14549866f903d6
talles <talles.lima@aluno.ufc.br>
2026-06-18 16:07:19 -0300

    adicionar viewmodel para ela do carrinho

--- artetoca/app/src/main/java/com/example/artetoca/carrinho/carrinho.kt
+++ artetoca/app/src/main/java/com/example/artetoca/carrinho/carrinho.kt
@@ -12,10 +12,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -23,30 +19,14 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.artetoca.R
-import java.util.UUID
-import kotlin.random.Random
-
-val itemsMock = listOf(
-    ItemCardData(R.drawable._1, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._2, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._3, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._4, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._5, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._6, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._7, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._8, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._9, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._10, Random.nextInt(10, 150), 1),
-    ItemCardData(R.drawable._11, Random.nextInt(10, 150), 1),
-)
 
 @Composable
 fun Carrinho(modifier: Modifier = Modifier) {
-    var items by remember { mutableStateOf(itemsMock) }
-    val removeItem = { id: UUID ->
-        items = items.filter { item -> item.id != id }
-    }
+
+    val viewModel: CarrinhoViewModel = viewModel()
+
     Column {
         Row(
             modifier = modifier
@@ -73,8 +53,17 @@ fun Carrinho(modifier: Modifier = Modifier) {
                 .padding(16.dp),
             verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            ItemList(items, removeItem, Modifier.weight(1f))
-            FinalizerCard(items.sumOf { it.preco }.toFloat())
+            ItemList(
+                viewModel.items,
+                { viewModel.removeItem(it) },
+                { id, qtd -> viewModel.updateItem(id, qtd + 1) },
+                { id, qtd ->
+                    if (qtd == 1) viewModel.removeItem(id)
+                    else viewModel.updateItem(id, qtd - 1)
+                },
+                Modifier.weight(1f)
+            )
+            FinalizerCard(viewModel.items.sumOf { it.preco * it.qtd }.toFloat())
         }
     }
 }
