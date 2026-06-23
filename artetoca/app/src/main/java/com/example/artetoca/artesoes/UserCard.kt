854cd6613fbe879aba02ff2bc38782fa2e9c9946
talles <talles.lima@aluno.ufc.br>
2026-06-23 10:15:02 -0300

    criar pacote artesoes

--- artetoca/app/src/main/java/com/example/artetoca/artesoes/UserCard.kt
+++ artetoca/app/src/main/java/com/example/artetoca/artesoes/UserCard.kt
@@ -1,32 +1,27 @@
-package com.example.artetoca
+package com.example.artetoca.artesoes
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-
-val cards = listOf(
-    Triple("titulo", "categoria", "descriçao"),
-    Triple("titulo", "categoria", "descriçao"),
-    Triple("titulo", "categoria", "descriçao"),
-    Triple("titulo", "categoria", "descriçao"),
-    Triple("titulo", "categoria", "descriçao"),
-    Triple("titulo", "categoria", "descriçao")
-)
+import com.example.artetoca.R.drawable
 
 @Composable
 fun UserCard(
@@ -49,11 +44,14 @@ fun UserCard(
                 .padding(10.dp)
                 .background(Color.White)
         ) {
-            AsyncImage(
-                model = "https://img.icons8.com/3d-fluent/100/user-2.png",
-                contentDescription = null,
-                modifier = Modifier.size(100.dp)
-            )
+            Box(
+                modifier = Modifier
+                    .size(60.dp)
+                    .background(Color.Gray, shape = CircleShape)
+                    .padding(8.dp)
+            ) {
+                Image(painter = painterResource(id = drawable.artetoca), contentDescription = null)
+            }
 
             Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
                 Text(text = titulo, fontSize = 16.sp, fontWeight = FontWeight.Bold)
@@ -66,11 +64,7 @@ fun UserCard(
 
 @Composable
 fun CardMock(modifier: Modifier = Modifier) {
-    Column(modifier) {
-        cards.forEach {
-            UserCard(it.first, it.second, it.third)
-        }
-    }
+       UserCard("titulo", "categoria", "descriçao")
 }
 
 @Preview
