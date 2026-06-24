3403798fb79769bc03e01fb0341ebd3db147cc71
Talles andré <tallesandre15@gmail.com>
2026-06-23 22:40:59 -0300

    definir preto para cor padrão de todas as fontes

--- artetoca/app/src/main/java/com/example/artetoca/ui/theme/Theme.kt
+++ artetoca/app/src/main/java/com/example/artetoca/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.artetoca.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -9,18 +8,21 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
+    tertiary = Pink80,
+    onBackground = Color.Black
 )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    onBackground = Color.Black
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
