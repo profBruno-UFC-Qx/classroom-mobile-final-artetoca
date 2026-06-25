4840da8437164a9575d930b38cb54ae17463a705
Luana <lopes.luana6@gmail.com>
2026-06-25 15:02:05 -0300

    icone nas categorias

--- artetoca/app/src/main/java/com/example/artetoca/Categorias.kt
+++ artetoca/app/src/main/java/com/example/artetoca/Categorias.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -53,11 +56,18 @@ fun CategoriaVendas(
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
+        Icon(
+            painter = painterResource(id = R.drawable.heart2),
+            modifier = Modifier.size(18.dp),
+            tint = RosaEscuro,
+            contentDescription = null
+        )
         Text (
             text = nome,
             fontSize = 15.sp,
             fontWeight = FontWeight.Light,
             color = RosaEscuro
         )
+
     }
 }
