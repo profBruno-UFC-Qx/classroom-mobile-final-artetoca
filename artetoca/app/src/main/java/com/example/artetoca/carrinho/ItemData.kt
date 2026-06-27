32f3fe5a7b3a676a01835b7721a81781806de210
Luana <lopes.luana6@gmail.com>
2026-06-27 14:31:33 -0300

    atualizacao na parte do carrinho

--- artetoca/app/src/main/java/com/example/artetoca/carrinho/ItemData.kt
+++ artetoca/app/src/main/java/com/example/artetoca/carrinho/ItemData.kt
@@ -3,8 +3,9 @@ package com.example.artetoca.carrinho
 import java.util.UUID
 
 data class ItemCardData(
+    val nome: String,
     val img: Int,
-    val preco: Int,
+    val preco: Float,
     val qtd: Int
 ) {
     val id: UUID = UUID.randomUUID()
