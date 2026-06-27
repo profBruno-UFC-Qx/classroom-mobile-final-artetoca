87caa92900e6f72e94684ee2a28141ba3cca29b1
Luana <lopes.luana6@gmail.com>
2026-06-27 14:37:53 -0300

    atualizacao na parte do carrinho

--- artetoca/app/src/main/java/com/example/artetoca/carrinho/CarrinhoViewModel.kt
+++ artetoca/app/src/main/java/com/example/artetoca/carrinho/CarrinhoViewModel.kt
@@ -2,25 +2,24 @@ package com.example.artetoca.carrinho
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
-import com.example.artetoca.R
 import java.util.UUID
-import kotlin.random.Random
+
 
 class CarrinhoViewModel : ViewModel() {
 
-    val items = mutableStateListOf(
-        ItemCardData(R.drawable._1, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._2, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._3, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._4, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._5, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._6, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._7, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._8, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._9, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._10, Random.nextInt(10, 150), 1),
-        ItemCardData(R.drawable._11, Random.nextInt(10, 150), 1),
-    )
+    val items = mutableStateListOf<ItemCardData>()
+
+    fun adicionarItem(produto : Produto) {
+        val precoFloat = produto.preco
+            .toFloatOrNull() ?: Of
+
+        val index = items.indexOfFirst { it.nome == produto.nome && it.img == produto.imagem }
+        if(index != -1) {
+            items[index] = items[index].copy( qtd = items[index].qtd + 1)
+        } else {
+            items.add(ItemCardData(produto.nome, produto.imagem, precoFloat, 1))
+        }
+    }
 
     fun updateItem(id: UUID, novaQuantidade: Int) {
         val index = items.indexOfFirst { it.id == id }
