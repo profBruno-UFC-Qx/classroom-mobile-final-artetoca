c43540001e7775c29d2fbf805e857c9d87a845dc
Talles andré <tallesandre15@gmail.com>
2026-06-28 20:49:26 -0300

    possibilitar scroll

--- artetoca/app/src/main/java/com/example/artetoca/carrinho/EmptyCarMenssage.kt
+++ artetoca/app/src/main/java/com/example/artetoca/carrinho/EmptyCarMenssage.kt
@@ -9,7 +9,9 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
@@ -32,8 +34,10 @@ fun EmptyCartMessage(
     onExploreClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+    val scrollState = rememberScrollState()
     Column(
         modifier = modifier
+            .verticalScroll(scrollState)
             .fillMaxSize()
             .background(Background)
             .padding(24.dp),
