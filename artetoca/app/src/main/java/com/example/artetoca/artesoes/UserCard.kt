36176c26dfe64107936ad0546c0dc29014eac5f7
Talles andré <tallesandre15@gmail.com>
2026-06-28 14:06:39 -0300

    dixar aparencia do card igual ao figma

--- artetoca/app/src/main/java/com/example/artetoca/artesoes/UserCard.kt
+++ artetoca/app/src/main/java/com/example/artetoca/artesoes/UserCard.kt
@@ -1,17 +1,20 @@
 package com.example.artetoca.artesoes
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -21,33 +24,31 @@ import coil.compose.AsyncImage
 @Composable
 fun UserCard(
     titulo: String,
-    descrição: String,
+    descricao: String,
+    especialidade: String,
     img_url: String,
     modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(16.dp),
-        colors = CardDefaults.cardColors(
-            containerColor = Color.White
-        )
+        modifier = modifier.fillMaxWidth(),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
     ) {
         Row(
             horizontalArrangement = Arrangement.spacedBy(12.dp),
-            modifier = Modifier
-                .padding(10.dp)
-                .background(Color.White)
+            modifier = Modifier.padding(16.dp)
         ) {
             AsyncImage(
                 model = img_url,
-                contentDescription = null
+                contentDescription = null,
+                modifier = Modifier
+                    .size(80.dp)
+                    .clip(CircleShape),
+                contentScale = ContentScale.Crop
             )
-
             Column(verticalArrangement = Arrangement.spacedBy(8.dp)) {
                 Text(text = titulo, fontSize = 16.sp, fontWeight = FontWeight.Bold)
-                Text(text = "especialidade", color = Color(0xffe88ba0))
-                Text(text = descrição, color = Color(0xff6d6d6d), fontSize = 13.sp)
+                Text(text = especialidade, color = Color(0xffe88ba0))
+                Text(text = descricao, color = Color(0xff6d6d6d), fontSize = 13.sp)
             }
         }
     }
@@ -55,7 +56,7 @@ fun UserCard(
 
 @Composable
 fun CardMock(modifier: Modifier = Modifier) {
-    UserCard("titulo", "categoria", "descriçao")
+    UserCard("titulo", "categoria", "especialiade","descriçao")
 }
 
 @Preview
